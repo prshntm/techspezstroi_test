0dec6039994c88dcf4de7b48699817a216d176bf
Tamara <tamara.parshina@gmail.com>
2021-09-06 12:04:43 +1000

    second commit

--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -11,3 +11,7 @@ const swiper = new Swiper('.swiper', {
 			  },
 
 			});
+const swiper = document.querySelector('.swiper').swiper;
+
+// Now you can use all slider methods like
+swiper.slideNext();
